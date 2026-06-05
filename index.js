bb189b9e1a99ff2a82f305cb41139e79ddf0906a
{Karan <karanattri022@gmail.com>
2026-06-05 08:48:11 +0530

    initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/" , (req , res)=>{
+    res.json({
+        message : "Server running successfully"
+    })
+});
+
+app.listen(3000 , ()=>{
+    console.log("server running successfully on port 3000")
+});
